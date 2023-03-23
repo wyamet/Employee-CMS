54f8f3bf152b41c6eec55dc5f4cc6c3fd4b546fa
wyamet <wyattdomanski.md@gmail.com>
2023-03-22 22:30:19 -0700

    fixed errors, got main questions working

--- Main/db/Db.js
+++ Main/db/Db.js
@@ -26,6 +26,68 @@ class Db {
     });
   }
 
+  viewAllDepartments() {
+    const sql = "SELECT * FROM department";
+    return this.connection
+      .promise()
+      .query(sql)
+      .then(([rows]) => rows);
+  }
+
+  viewAllRoles() {
+    const sql = "SELECT * FROM role";
+    return this.connection
+      .promise()
+      .query(sql)
+      .then(([rows]) => rows);
+  }
+
+  viewAllEmployees() {
+    const sql = `
+      SELECT
+        e.id,
+        e.first_name,
+        e.last_name,
+        r.title AS role_title,
+        d.name AS department_name,
+        r.salary,
+        CONCAT(m.first_name, ' ', m.last_name) AS manager_name
+      FROM employee e
+      JOIN role r ON e.role_id = r.id
+      JOIN department d ON r.department_id = d.id
+      LEFT JOIN employee m ON e.manager_id = m.id`;
+    return this.connection
+      .promise()
+      .query(sql)
+      .then(([rows]) => rows);
+  }
+
+  addDepartment(name) {
+    const sql = "INSERT INTO department (name) VALUES (?)";
+    return this.connection.promise().execute(sql, [name]);
+  }
+
+  addRole(title, salary, departmentId) {
+    const sql =
+      "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)";
+    return this.connection
+      .promise()
+      .execute(sql, [title, salary, departmentId]);
+  }
+
+  addEmployee(firstName, lastName, roleId, managerId) {
+    const sql =
+      "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
+    return this.connection
+      .promise()
+      .execute(sql, [firstName, lastName, roleId, managerId]);
+  }
+
+  updateEmployeeRole(employeeId, roleId) {
+    const sql = "UPDATE employee SET role_id = ? WHERE id = ?";
+    return this.connection.promise().execute(sql, [roleId, employeeId]);
+  }
+
   endConnection() {
     this.connection.end();
     console.log("Connection ended");
--- Main/index.js
+++ Main/index.js
@@ -26,30 +26,143 @@ const init = async () => {
         }
         break;
       case "viewAllRoles":
-        // Handle viewAllRoles option
+        try {
+          const roles = await db.viewAllRoles();
+          console.table(roles);
+        } catch (error) {
+          console.log(error);
+        }
         break;
       case "viewAllEmployees":
-        // Handle viewAllEmployees option
+        try {
+          const employees = await db.viewAllEmployees();
+          console.table(employees);
+        } catch (error) {
+          console.log(error);
+        }
         break;
       case "addDepartment":
-        // Handle addDepartment option
+        try {
+          const department = await prompt([
+            {
+              type: "input",
+              name: "name",
+              message: "Enter department name:",
+            },
+          ]);
+          await db.addDepartment(department.name);
+          console.log(`Added ${department.name} department to the database`);
+        } catch (error) {
+          console.log(error);
+        }
         break;
+
       case "addRole":
-        // Handle addRole option
+        try {
+          const departments = await db.viewAllDepartments();
+          const role = await prompt([
+            {
+              type: "input",
+              name: "title",
+              message: "Enter role title:",
+            },
+            {
+              type: "input",
+              name: "salary",
+              message: "Enter role salary:",
+            },
+            {
+              type: "list",
+              name: "department_id",
+              message: "Choose department:",
+              choices: departments.map(({ id, name }) => ({
+                name,
+                value: id,
+              })),
+            },
+          ]);
+          await db.addRole(role.title, role.salary, role.department_id);
+          console.log(`Added ${role.title} role to the database`);
+        } catch (error) {
+          console.log(error);
+        }
         break;
+
       case "addEmployee":
-        // Handle addEmployee option
+        try {
+          const employeeData = await prompt([
+            {
+              type: "input",
+              name: "firstName",
+              message: "What is the employee's first name?",
+            },
+            {
+              type: "input",
+              name: "lastName",
+              message: "What is the employee's last name?",
+            },
+            {
+              type: "list",
+              name: "roleId",
+              message: "What is the employee's role?",
+              choices: (
+                await db.viewAllRoles()
+              ).map((role) => ({ name: role.title, value: role.id })),
+            },
+            {
+              type: "list",
+              name: "managerId",
+              message: "Who is the employee's manager?",
+              choices: (
+                await db.viewAllEmployees()
+              ).map((employee) => ({
+                name: `${employee.first_name} ${employee.last_name}`,
+                value: employee.id,
+              })),
+            },
+          ]);
+          await db.addEmployee(
+            employeeData.firstName,
+            employeeData.lastName,
+            employeeData.roleId,
+            employeeData.managerId
+          );
+          console.log(
+            `Employee added: ${employeeData.firstName} ${employeeData.lastName}`
+          );
+        } catch (error) {
+          console.log(error);
+        }
         break;
       case "updateEmployeeRole":
-        // Handle updateEmployeeRole option
+        try {
+          const employeeData = await prompt([
+            {
+              type: "list",
+              name: "id",
+              message: "Which employee's role do you want to update?",
+              choices: await db.viewAllEmployees(),
+            },
+            {
+              type: "list",
+              name: "roleId",
+              message: "What is the employee's new role?",
+              choices: await db.viewAllRoles(),
+            },
+          ]);
+          await db.updateEmployeeRole(employeeData.id, employeeData.roleId);
+          console.log(`Employee role updated`);
+        } catch (error) {
+          console.log(error);
+        }
         break;
-      case "end":
-        console.log("App has stopped");
+      case "exit":
         isRunning = false;
-        await db.endConnection();
-        break;
+        await db.closeConnection();
+        process.exit(0);
       default:
-        console.log("Invalid option");
+        console.log("Invalid option selected");
+        break;
     }
   }
 };
